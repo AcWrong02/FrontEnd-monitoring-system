a1995eab274756834a4d30f6f87b35dbae5ea864
AcWrong02 <3024800887@qq.com>
2024-01-05 23:37:01 +0800

    添加全局事件处理函数on

--- packages/utils/src/helpter.ts
+++ packages/utils/src/helpter.ts
@@ -1,5 +1,30 @@
 import { IAnyObject } from "packages/types/src/common";
 
+// 用到所有事件名称
+type TotalEventName =
+  | keyof GlobalEventHandlersEventMap
+  | keyof XMLHttpRequestEventTargetEventMap
+  | keyof WindowEventMap;
+
+/**
+ * 添加事件监听器
+ *
+ * ../export
+ * ../param {{ addEventListener: Function }} target
+ * ../param {keyof TotalEventName} eventName
+ * ../param {Function} handler
+ * ../param {(boolean | Object)} opitons
+ * ../returns
+ */
+export function on(
+  target: { addEventListener: Function },
+  eventName: TotalEventName,
+  handler: Function,
+  opitons: boolean | unknown = false,
+): void {
+  target.addEventListener(eventName, handler, opitons);
+}
+
 /**
  *
  * 重写对象上面的某个属性(一般是方法)
